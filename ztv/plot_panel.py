6aa6a815fba99c462ecb1f7c0206913362d416c9
Henry Roe <hroe@hroe.me>
2015-03-26 18:35:16 -0700

    have implemented basic line-draw on to slice plot in ztv.  (but not doing any plotting yet)

--- ztv/plot_panel.py
+++ ztv/plot_panel.py
@@ -1,17 +1,22 @@
 import wx
 from wx.lib.pubsub import Publisher
+from wx.lib.pubsub.core.datamsg import Message
 import matplotlib
 matplotlib.interactive(True)
 matplotlib.use('WXAgg')
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
+from matplotlib.patches import PathPatch
+from matplotlib.path import Path
 import numpy as np
+import sys
 
 
 class PlotPanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
         self.ztv_frame = self.GetTopLevelParent()
+        self.primary_image_patch = None
         self.figure = Figure(dpi=None, figsize=(1.,1.))
         self.axes = self.figure.add_subplot(111)
         self.canvas = FigureCanvasWxAgg(self, -1, self.figure)
@@ -23,7 +28,8 @@ class PlotPanel(wx.Panel):
         self.start_pt = wx.RealPoint(0., 0.)
         self.end_pt = wx.RealPoint(0., 0.)
         self.redraw()
-        Publisher().subscribe(self.update_line_plot_points, "update_line_plot_points")
+        Publisher().subscribe(self.on_new_xy0, "new_slice_plot_xy0")
+        Publisher().subscribe(self.on_new_xy1, "new_slice_plot_xy1")
         Publisher().subscribe(self.redraw, "primary_xy_limits-changed")
         Publisher().subscribe(self.redraw, "new-image-loaded")
 
@@ -37,14 +43,37 @@ class PlotPanel(wx.Panel):
         self.figure.set_size_inches(float(pixels[0])/self.figure.get_dpi(),
                                     float(pixels[1])/self.figure.get_dpi())
 
-    def update_line_plot_points(self, msg):
+    def on_new_xy0(self, msg):
         if isinstance(msg, Message):
-            pts = msg.data
+            x,y = msg.data
         else:
-            pts = msg
-        self.start_pt = wx.RealPoint(*pts[0])
-        self.end_pt = wx.RealPoint(*pts[1])
-        self.redraw()
+            x,y = msg
+        self.start_pt.x, self.start_pt.y = x, y
+        self.redraw_on_image()
+#         self.redraw()
+
+    def on_new_xy1(self, msg):    # HEREIAM  need to update once have gotten xy0 version working
+        if isinstance(msg, Message):
+            x,y = msg.data
+        else:
+            x,y = msg
+        self.end_pt.x, self.end_pt.y = x, y
+        self.redraw_on_image()
+#         self.redraw()
+
+    def redraw_on_image(self):
+        if self.primary_image_patch is not None:
+            self.ztv_frame.primary_image_panel.axes.patches.remove(self.primary_image_patch)
+        path = Path([self.start_pt, self.end_pt], [Path.MOVETO, Path.LINETO])
+        self.primary_image_patch = PathPatch(path, color='orange', lw=1)
+        self.ztv_frame.primary_image_panel.axes.add_patch(self.primary_image_patch)
+        self.ztv_frame.primary_image_panel.figure.canvas.draw()
+
+    def on_cancel(self, event):  # TODO link up to a button
+        if self.primary_image_patch is not None:
+            self.ztv_frame.primary_image_panel.axes.patches.remove(self.primary_image_patch)
+        self.ztv_frame.primary_image_panel.figure.canvas.draw()
+        self.primary_image_patch = None
 
   # HEREIAM
   # TODO: re-enable the default matplotlib zoom/etc controls for the plot panel
