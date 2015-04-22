3434dc4f57433cbaa2921666ce45747496c3c861
Henry Roe <hroe@hroe.me>
2015-04-22 12:07:56 -0700

    add link to versioning guidlinees, updated version number to 0.1.0.post1

--- ztv/__about__.py
+++ ztv/__about__.py
@@ -7,7 +7,8 @@ __title__ = "ztv"
 __summary__ = "Simple python image viewer, largely intended for astronomical applications"
 __uri__ = "https://github.com/henryroe/ztv"
 
-__version__ = "0.1.0"
+#   https://python-packaging-user-guide.readthedocs.org/en/latest/distributing.html#version
+__version__ = "0.1.0.post1"
 
 __author__ = "Henry Roe"
 __email__ = "hroe@hroe.me"
