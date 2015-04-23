a421334d0e6e901ba52b2a35090303463abf445c
Henry Roe <hroe@hroe.me>
2015-04-22 19:41:00 -0700

    added some additional comments on version numbering

--- ztv/__about__.py
+++ ztv/__about__.py
@@ -8,6 +8,11 @@ __summary__ = "Simple python image viewer, largely intended for astronomical app
 __uri__ = "https://github.com/henryroe/ztv"
 
 #   https://python-packaging-user-guide.readthedocs.org/en/latest/distributing.html#version
+# intended numbering convention during a release cycle is, e.g.  [decided this after some fumbling]
+#   0.1.1.dev      # during development of 0.1.1
+#   0.1.1.rc2      # shouldn't really be using much as I think I now have github/pypi figured out a little better
+#   0.1.1          # released
+#   0.1.1-3        # quick bug fixes
 __version__ = "0.1.0.post1"
 
 __author__ = "Henry Roe"
