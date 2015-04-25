97fcf063ddda32815fe58650cf130bdcb01c75cd
Henry Roe <hroe@hroe.me>
2015-04-25 13:19:40 -0700

    idea taken from astropy:  if 'dev' in version string, then append a unique ID (total # of commits on project). great idea, stole it the moment I saw it.

--- ztv/__about__.py
+++ ztv/__about__.py
@@ -1,3 +1,5 @@
+from astropy_helpers.git_helpers import get_git_devstr
+
 __all__ = [
     "__title__", "__summary__", "__uri__", "__version__", "__author__",
     "__email__", "__license__", "__copyright__",
@@ -7,13 +9,16 @@ __title__ = "ztv"
 __summary__ = "Simple python image viewer, largely intended for astronomical applications"
 __uri__ = "https://github.com/henryroe/ztv"
 
-#   https://python-packaging-user-guide.readthedocs.org/en/latest/distributing.html#version
-# intended numbering convention during a release cycle is, e.g.  [decided this after some fumbling]
-#   0.1.1.dev      # during development of 0.1.1
-#   0.1.1.rc2      # shouldn't really be using much as I think I now have github/pypi figured out a little better
-#   0.1.1          # released
-#   0.1.1-3        # quick bug fixes
-__version__ = "0.1.0.post1"
+# VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
+__version__ = "0.1.1.dev"
+
+VERSION = '1.1.dev'
+
+# Indicates if this version is a release version
+RELEASE = 'dev' not in VERSION
+
+if not RELEASE:
+    VERSION += get_git_devstr(False)
 
 __author__ = "Henry Roe"
 __email__ = "hroe@hroe.me"
