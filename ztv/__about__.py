df4b23ba4e0b75f1e72f7392d72b5e07e1caf3a8
Henry Roe <hroe@hroe.me>
2015-09-03 12:34:57 -0700

    made import of astropy_helpers.git_helpers optional so that doesn't fail during first install if astropy_helpers not yet installed

--- ztv/__about__.py
+++ ztv/__about__.py
@@ -1,5 +1,9 @@
-from astropy_helpers.git_helpers import get_git_devstr
-
+try:
+    from astropy_helpers.git_helpers import get_git_devstr
+    git_devstr = get_git_devstr(False)
+except ImportError:
+    git_devstr = ""
+    
 __all__ = [
     "__title__", "__summary__", "__uri__", "__version__", "__author__",
     "__email__", "__license__", "__copyright__",
@@ -16,7 +20,7 @@ __version__ = "0.1.3dev"
 RELEASE = 'dev' not in __version__
 
 if not RELEASE:
-    __version__ += get_git_devstr(False)
+    __version__ += ""
 
 __author__ = "Henry Roe"
 __email__ = "hroe@hroe.me"
